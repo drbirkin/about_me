f9d5dc01e823ba1c80c8f47a154d847a9b64177d
drbirkin <davidliat98@gmail.com>
2021-04-19 23:30:12 +0200

    adjust autoscroller effect

--- assets/fullpage.js
+++ assets/fullpage.js
@@ -1,5 +1,9 @@
+//First the variables our app is going to use need to be declared
+
+	//References to DOM elements
 	var $window = $(window);
 	var $document = $(document);
+	//Only links that starts with #
 	var $navButtons = $("nav a").filter("[href^=#]");
 	var $navGoPrev = $(".go-prev");
 	var $navGoNext = $(".go-next");
@@ -7,17 +11,25 @@
 	var $slides = $(".slide");
 	var $currentSlide = $slides.first();
 
+	//Animating flag - is our app animating
 	var isAnimating = false;
 
+	//The height of the window
 	var pageHeight = $window.innerHeight();
 
+	//Key codes for up and down arrows on keyboard. We'll be using this to navigate change slides using the keyboard
 	var keyCodes = {
 		UP  : 38,
 		DOWN: 40
 	}
 
+	//Going to the first slide
 	goToSlide($currentSlide);
 
+
+	/*
+	*   Adding event listeners
+	* */
 
 	$window.on("resize", onResize).resize();
 	$window.on("mousewheel DOMMouseScroll", onMouseWheel);
@@ -26,6 +38,14 @@
 	$navGoPrev.on("click", goToPrevSlide);
 	$navGoNext.on("click", goToNextSlide);
 
+	/*
+	*   Internal functions
+	* */
+
+
+	/*
+	*   When a button is clicked - first get the button href, and then slide to the container, if there's such a container
+	* */
 	function onNavButtonClick(event)
 	{
 		//The clicked button
@@ -42,6 +62,10 @@
 		}
 	}
 
+	/*
+	*   Getting the pressed key. Only if it's up or down arrow, we go to prev or next slide and prevent default behaviour
+	*   This way, if there's text input, the user is still able to fill it
+	* */
 	function onKeyDown(event)
 	{
 
@@ -60,6 +84,9 @@
 
 	}
 
+	/*
+	*   When user scrolls with the mouse, we have to change slides
+	* */
 	function onMouseWheel(event)
 	{
 		//Normalize event wheel delta
@@ -78,6 +105,9 @@
 		event.preventDefault();
 	}
 
+	/*
+	*   If there's a previous slide, slide to it
+	* */
 	function goToPrevSlide()
 	{
 		if($currentSlide.prev().length)
@@ -86,6 +116,9 @@
 		}
 	}
 
+	/*
+	*   If there's a next slide, slide to it
+	* */
 	function goToNextSlide()
 	{
 		if($currentSlide.next().length)
@@ -94,6 +127,9 @@
 		}
 	}
 
+	/*
+	*   Actual transition between slides
+	* */
 	function goToSlide($slide)
 	{
 		//If the slides are not changing and there's such a slide
@@ -114,23 +150,35 @@
 		}
 	}
 
+	/*
+	*   Once the sliding is finished, we need to restore "isAnimating" flag.
+	*   You can also do other things in this function, such as changing page title
+	* */
 	function onSlideChangeEnd()
 	{
 		isAnimating = false;
 	}
 
+	/*
+	*   When user resize it's browser we need to know the new height, so we can properly align the current slide
+	* */
 	function onResize(event)
 	{
 
 		//This will give us the new height of the window
 		var newPageHeight = $window.innerHeight();
 
+		/*
+		*   If the new height is different from the old height ( the browser is resized vertically ), the slides are resized
+		* */
 		if(pageHeight !== newPageHeight)
 		{
 			pageHeight = newPageHeight;
 
+			//This can be done via CSS only, but fails into some old browsers, so I prefer to set height via JS
 			TweenLite.set([$slidesContainer, $slides], {height: pageHeight + "px"});
 
+			//The current slide should be always on the top
 			TweenLite.set($slidesContainer, {scrollTo: {y: pageHeight * $currentSlide.index() }});
 		}
 
